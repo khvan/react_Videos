2b243a3f5fe13bdf68b995a590bde4e872ebe4e7
kBoris <khvanboris@gmail.com>
2019-09-27 12:58:53 -0700

    initialized a react prject and created SearchBar components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = {term: ""}
+  onInputChange = (event)=>{
+    this.setState({term: event.target.value})
+  }
+  onSumbit = event => {
+    
+
+  }
+  render () {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field" />
+            <label>Video Search</label>
+              <input 
+              onChange = {this.onInputChange}
+              onSubmit ={this.onSubmit}
+              type = 'text' 
+              value ={this.state.term} 
+              />
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
