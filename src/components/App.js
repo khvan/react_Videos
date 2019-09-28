6da0e27c9cffa5803d05fb23920f83786dcc4c30
kBoris <khvanboris@gmail.com>
2019-09-27 21:34:50 -0700

    added rendering of an embeded youtube player to display searched videos

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube'
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component {
-  state = {videos: []}
+  state = {videos: [], selectedVideos: null}
+
+  onVideoSelect = (video)=>{
+    this.setState({selectedVideos: video})
+  }
+
   onTermSubmit = async (term)=>{
    const response = await youtube.get('/search', {
        params: { 
@@ -15,10 +21,18 @@ class App extends React.Component {
     this.setState({videos: response.data.items})
   }
   render () {
+    
     return (
     <div className="ui container">
       <SearchBar onFormSubmit = {this.onTermSubmit} />
-      <div><VideoList videos={this.state.videos} /></div>
+      
+        <VideoDetail video = {this.state.selectedVideos}/>
+      
+      <div>
+        <VideoList 
+        videos={this.state.videos} 
+        onVideoSelect = {this.onVideoSelect}
+        /></div>
     </div>
     
     )
